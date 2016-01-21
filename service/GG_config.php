7da648918b3ccd77099e8ed9b920e6d22dc522cf
Antonio Sotto Jr <antoniosottojr@gmail.com>
2016-01-21 10:21:36 +0800

    GG_config password
    Removed DB_PASSWORD password value since Anton's XAMPP doesn't have
    password. :P

--- service/GG_config.php
+++ service/GG_config.php
@@ -13,6 +13,6 @@
 
 	define("DB_HOST", "localhost");
 	define("DB_USER", "root");
-	define("DB_PASSWORD", "goldengarbage");
+	define("DB_PASSWORD", "");
 	define('DB_DATABASE', "db_goldengarbage");
 ?>
